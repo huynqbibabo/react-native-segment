7d639e15cbafff62088319436c735340cebf287b
Quang Huy <nqhuy1175@gmail.com>
2021-03-27 17:19:52 +0700

    fix: ios export method

--- ios/Segment.m
+++ ios/Segment.m
@@ -6,9 +6,7 @@ RCT_EXPORT_MODULE()
 
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(getFacebookAdCampaignId,
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(getFacebookAdCampaignId:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 {
   resolve(_FacebookAdCampainId);
 }
